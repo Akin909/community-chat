abd13d6378c0be7122122895eafb667128a502a8
Akin909 <akin.sowemimo@gmail.com>
2017-04-16 14:32:32 +0100

    Add a form to login and handlers for user input

--- src/components/login.js
+++ src/components/login.js
@@ -11,23 +11,45 @@ class Login extends Component {
 
   constructor(props) {
     super(props);
+
+    this.handleLogin = this.handleLogin.bind(this);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  handlePasswordInput(event) {
+    console.log(event.target.value);
+  }
+
+  handleLogin(event) {
+    event.preventDefault();
+    this.setState({
+      username: this.state.username,
+      password: this.state.password,
+    });
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleLogin}>
         <label htmlFor="username">Username: </label>
         <input
           type="text"
           name="username"
           placeholder="Please enter a user name"
+          onChange={this.handleUsernameInput}
         />
         <label htmlFor="password">Password: </label>
         <input
           type="text"
           name="password"
           placeholder="Please enter a password"
+          onChange={this.handlePasswordInput}
         />
+        <input type="submit" value="submit" />
       </form>
     );
   }
