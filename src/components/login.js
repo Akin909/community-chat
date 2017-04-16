d64f824d0447ca99c21db37633bdbcf2c1f74cbc
Akin909 <akin.sowemimo@gmail.com>
2017-04-16 15:05:10 +0100

    Add controlled input forms required attributes
    Add a successful registration message

--- src/components/login.js
+++ src/components/login.js
@@ -13,26 +13,48 @@ class Login extends Component {
     super(props);
 
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleUsernameInput = this.handleUsernameInput.bind(this);
+    this.handlePasswordInput = this.handlePasswordInput.bind(this);
 
     this.state = {
       username: '',
       password: '',
+      submitted: false,
     };
   }
 
+  handleUsernameInput(event) {
+    console.log(event.target.value);
+    this.setState({
+      username: event.target.value,
+    });
+  }
   handlePasswordInput(event) {
     console.log(event.target.value);
+    this.setState({
+      password: event.target.value,
+    });
   }
 
   handleLogin(event) {
     event.preventDefault();
+    if (this.state.submitted) {
+      return;
+    }
     this.setState({
+      submitted: true,
       username: this.state.username,
       password: this.state.password,
     });
+    console.log('state', this.state);
   }
 
   render() {
+    if (this.state.submitted) {
+      return (
+        <div>You have successfully registered in {this.state.username}</div>
+      );
+    }
     return (
       <form onSubmit={this.handleLogin}>
         <label htmlFor="username">Username: </label>
@@ -41,6 +63,8 @@ class Login extends Component {
           name="username"
           placeholder="Please enter a user name"
           onChange={this.handleUsernameInput}
+          value={this.state.username}
+          required
         />
         <label htmlFor="password">Password: </label>
         <input
@@ -48,6 +72,8 @@ class Login extends Component {
           name="password"
           placeholder="Please enter a password"
           onChange={this.handlePasswordInput}
+          value={this.state.password}
+          required
         />
         <input type="submit" value="submit" />
       </form>
